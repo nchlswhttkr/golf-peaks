d202b1fd53d5ab43ff5da14caf8eaa1e2a2b5e3d
Nicholas Whittaker <nicholas@nicholas.cloud>
2020-08-21 16:13:35 +1000

    Add logic for slopes/walls
    It's not pretty, but it works. Cleanup will happen eventually...

--- run.sh
+++ run.sh
@@ -2,7 +2,28 @@
 
 set -euo pipefail
 
-ls -1 levels/* | while read LEVEL; do
+osascript -e '
+activate application "Golf Peaks"
+'
+
+ls -1 levels/01-0* | while read LEVEL; do
+    cargo run -q -- --applescript < $LEVEL | osascript -i
+    sleep 3
+done;
+
+# complete challenge levels
+osascript -e '
+tell application "System Events" to keystroke "a"
+delay 0.1
+tell application "System Events" to keystroke "a"
+delay 0.1
+tell application "System Events" to keystroke "a"
+delay 0.1
+tell application "System Events" to key code 36
+delay 3
+'
+
+ls -1 levels/01-1* | while read LEVEL; do
     cargo run -q -- --applescript < $LEVEL | osascript -i
     sleep 3
 done;
--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use std::io;
 enum Terrain {
     Hole,
     Ground,
+    Slope(Direction),
     // Trap,
     // Quicksand,
     // Water,
@@ -24,7 +25,7 @@ enum Terrain {
 
 struct Tile {
     terrain: Terrain,
-    // elevation: i32,
+    elevation: i32,
     // corner: Option<Corner>,
 }
 
@@ -33,7 +34,7 @@ pub struct Move {
     distance: i32,
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Copy)]
 pub enum Direction {
     Up,
     Down,
@@ -62,7 +63,7 @@ fn interpret_map_and_moves(
                 },
                 Tile {
                     terrain: Terrain::Hole,
-                    // elevation: items[3].parse::<i32>().unwrap(),
+                    elevation: items.get(3).unwrap_or(&"0").parse::<i32>().unwrap(),
                     // corner: None,
                 },
             );
@@ -74,7 +75,25 @@ fn interpret_map_and_moves(
                 },
                 Tile {
                     terrain: Terrain::Ground,
-                    // elevation: items[3].parse::<i32>().unwrap(),
+                    elevation: items.get(3).unwrap_or(&"0").parse::<i32>().unwrap(),
+                    // corner: None,
+                },
+            );
+        } else if items[0] == "slope" {
+            map.insert(
+                Location {
+                    x: items[1].parse::<i32>().unwrap(),
+                    y: items[2].parse::<i32>().unwrap(),
+                },
+                Tile {
+                    terrain: Terrain::Slope(match items[4] {
+                        "up" => Direction::Up,
+                        "down" => Direction::Down,
+                        "left" => Direction::Left,
+                        "right" => Direction::Right,
+                        _ => panic!("Unknown slope direction"),
+                    }),
+                    elevation: items.get(3).unwrap_or(&"0").parse::<i32>().unwrap(),
                     // corner: None,
                 },
             );
@@ -143,15 +162,70 @@ fn try_move(
 ) -> Option<Location> {
     let mut move_copy = chosen_move.clone();
     let mut position_copy = position.clone();
-    let cur_tile = map.get(&position_copy);
-    while move_copy.distance > 0 && cur_tile.is_some() {
-        match chosen_direction {
-            Direction::Up => position_copy.y += 1,
-            Direction::Down => position_copy.y -= 1,
-            Direction::Left => position_copy.x -= 1,
-            Direction::Right => position_copy.x += 1,
+    let mut direction = chosen_direction.clone();
+    let mut cur_tile = map.get(&position_copy);
+    let mut in_bounds = cur_tile.is_some();
+    let mut stopped = move_copy.distance <= 0
+        && match cur_tile.unwrap().terrain {
+            Terrain::Hole => true,
+            Terrain::Ground => true,
+            Terrain::Slope(_) => false,
+        };
+    while in_bounds && !stopped {
+        // identify potential next position
+        let mut next_position = position_copy.clone();
+        match direction {
+            Direction::Up => next_position.y += 1,
+            Direction::Down => next_position.y -= 1,
+            Direction::Left => next_position.x -= 1,
+            Direction::Right => next_position.x += 1,
         }
+
+        // move
         move_copy.distance -= 1;
+        if let Some(next_tile) = map.get(&next_position) {
+            if cur_tile.unwrap().elevation >= next_tile.elevation {
+                position_copy = next_position;
+                if let Some(slope_dir) = match next_tile.terrain {
+                    Terrain::Slope(d) => Some(d),
+                    _ => None,
+                } {
+                    direction = slope_dir;
+                }
+            } else if cur_tile.unwrap().elevation == next_tile.elevation - 1
+                && move_copy.distance > 0 //has not stopped
+                && match next_tile.terrain {
+                    Terrain::Slope(slope_dir) => match slope_dir {
+                        Direction::Up => direction == Direction::Down,
+                        Direction::Down => direction == Direction::Up,
+                        Direction::Left => direction == Direction::Right,
+                        Direction::Right => direction == Direction::Left,
+                    },
+                    _ => false,
+                }
+            {
+                position_copy = next_position;
+            } else {
+                // turn around
+                direction = match direction {
+                    Direction::Up => Direction::Down,
+                    Direction::Down => Direction::Up,
+                    Direction::Left => Direction::Right,
+                    Direction::Right => Direction::Left,
+                }
+            }
+        } else {
+            return None; // move falls out of bounds
+        }
+
+        cur_tile = map.get(&position_copy);
+        in_bounds = cur_tile.is_some();
+        stopped = move_copy.distance <= 0
+            && match cur_tile.unwrap().terrain {
+                Terrain::Hole => true,
+                Terrain::Ground => true,
+                Terrain::Slope(_) => false,
+            };
     }
     // println!("{} {}", position_copy.x, position_copy.y);
     return Some(position_copy);
@@ -177,10 +251,6 @@ fn main() {
     let generate_applescript: bool = std::env::args()
         .find(|arg| arg == "--applescript")
         .is_some();
-    if generate_applescript {
-        println!("activate application \"Golf Peaks\"");
-        println!("delay 0.1");
-    }
     if let Some(solution_moves) = try_moves_to_reach_hole(&map, &Location { x: 0, y: 0 }, &moves) {
         let mut last_index = 0;
         for (i, direction) in solution_moves {
@@ -220,7 +290,11 @@ fn main() {
                 );
             }
             moves.remove(i as usize);
-            last_index = i - 1
+            if i > 0 {
+                last_index = i - 1;
+            } else {
+                last_index = 0;
+            }
         }
     } else {
         std::process::exit(1);
