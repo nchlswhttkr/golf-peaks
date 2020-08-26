3b92f8f393f760299b976fe843aad1e4256981b2
Nicholas Whittaker <nicholas@nicholas.cloud>
2020-08-26 18:02:56 +1000

    Add level-filename map, update scripts

---
+++ full-run.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+set -eu
+
+function complete_level {
+    $SHELL run.sh $1
+    sleep 2
+}
+
+function next_world {
+    osascript -e '
+tell application "System Events" to key code 36
+delay 2
+'
+}
+
+function challenge_levels {
+    osascript -e '
+tell application "System Events" to keystroke "a"
+delay 0.05
+tell application "System Events" to keystroke "a"
+delay 0.05
+tell application "System Events" to keystroke "a"
+delay 0.05
+tell application "System Events" to key code 36
+delay 2
+'
+}
+
+complete_level 01-01
+complete_level 01-02
+complete_level 01-03
+complete_level 01-04
+complete_level 01-05
+complete_level 01-06
+complete_level 01-07
+complete_level 01-08
+complete_level 01-09
+challenge_levels
+complete_level 01-10
+complete_level 01-11
+complete_level 01-12
+next_world
+complete_level 02-01
+complete_level 02-02
+complete_level 02-03
+complete_level 02-04
+complete_level 02-05
+complete_level 02-06
+complete_level 02-07
+complete_level 02-08
+complete_level 02-09
+challenge_levels
+complete_level 02-10
+complete_level 02-11
+complete_level 02-12
+next_world
+complete_level 03-01
+complete_level 03-02
+complete_level 03-03
+complete_level 03-04
+complete_level 03-05
+complete_level 03-06
+complete_level 03-07
+complete_level 03-08
+complete_level 03-09
+challenge_levels
+complete_level 03-10
+complete_level 03-11
+complete_level 03-12
+next_world
+complete_level 04-01
+complete_level 04-02
+complete_level 04-03
+complete_level 04-04
+complete_level 04-05
+complete_level 04-06
+complete_level 04-07
+complete_level 04-08
+complete_level 04-09
+challenge_levels
+complete_level 04-10
+complete_level 04-11
+complete_level 04-12
+next_world
+complete_level 05-01
+complete_level 05-02
+complete_level 05-03
+complete_level 05-04
+complete_level 05-05
+complete_level 05-06
+complete_level 05-07
+complete_level 05-08
+complete_level 05-09
+challenge_levels
+complete_level 05-10
+complete_level 05-11
+complete_level 05-12
+next_world
+complete_level 06-01
+complete_level 06-02
+complete_level 06-03
+complete_level 06-04
+complete_level 06-05
+complete_level 06-06
+complete_level 06-07
+complete_level 06-08
+complete_level 06-09
+challenge_levels
+complete_level 06-10
+complete_level 06-11
+complete_level 06-12
+next_world
+complete_level 07-01
+complete_level 07-02
+complete_level 07-03
+complete_level 07-04
+complete_level 07-05
+complete_level 07-06
+complete_level 07-07
+complete_level 07-08
+complete_level 07-09
+complete_level 07-10
+complete_level 07-11
+complete_level 07-12
+next_world
+complete_level 08-01
+complete_level 08-02
+complete_level 08-03
+complete_level 08-04
+complete_level 08-05
+complete_level 08-06
+complete_level 08-07
+complete_level 08-08
+complete_level 08-09
+challenge_levels
+complete_level 08-10
+complete_level 08-11
+complete_level 08-12
+complete_level 07-12
+next_world
+complete_level 09-01
+complete_level 09-02
+complete_level 09-03
+complete_level 09-04
+complete_level 09-05
+complete_level 09-06
+complete_level 09-07
+complete_level 09-08
+complete_level 09-09
+challenge_levels
+complete_level 09-10
+complete_level 09-11
+complete_level 09-12
